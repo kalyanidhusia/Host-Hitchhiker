0fd495973215b68a4e2cae22baa775c414bbbe4b
kalyanidhusia <kalyanidhusia.bhu@gmail.com>
2026-02-17 19:37:54 -0600

    #updates

--- submit_rnaseq.sh
+++ submit_rnaseq.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=58G
 #SBATCH --time=28:00:00
-#SBATCH --partition=xeon
+#SBATCH --partition=any
 
 # Create logs directory if it doesn't exist
 mkdir -p logs
@@ -17,19 +17,20 @@ module load apptainer/1.3.6-s2en2qm
 ## Run the pipeline
 #update gtf for viral entry
 nextflow run nf-core/rnaseq \
-    -r 3.14.0 \
-    -profile apptainer \
-    --input ./newsamplsheet.csv \
-    --outdir ./results_kshv_host \
-    --fasta /home/kdhusia/PROJECTS/manzo/data/human_kshv_combined.fasta \
-    --gtf /home/kdhusia/PROJECTS/manzo/data/human_kshv_final_fixed.gtf \
-    --strandedness reverse \
-    --skip_check_strandedness \
-    --skip_gtf_filter \
-    --skip_dupradar \
-    --skip_qualimap \
-    --aligner star_salmon \
-    --max_cpus 16 \
-    --max_memory '58.GB' \
-    --skip_biotype_qc \
-    -resume
+  -r 3.14.0 \
+  -profile apptainer \
+  --input dual_virus_samplesheet.csv \
+  --outdir results_host_multiVirus \
+  --fasta ref/human_virus.fasta \
+  --gtf ref/human_virus.gtf \
+  --strandedness reverse \
+  --skip_check_strandedness \
+  --skip_gtf_filter \
+  --skip_dupradar \
+  --skip_qualimap \
+  --aligner star_salmon \
+  --max_cpus 16 \
+  --max_memory '58.GB' \
+  --skip_biotype_qc \
+  -resume
+
